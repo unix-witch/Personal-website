3194eaceaa0bf4af663c9319d8307fa9dd6e5f7f
bsd_witch <77200446+unix-witch@users.noreply.github.com>
2022-05-03 08:18:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,11 @@
-const fs = require("fs");
 const express = require("express");
-
 let app = express();
+
 app.use(express.static('views'));
-app.use(express.json());
 
-app.get('/', function(req, res){ res.sendFile("index.html", {root: __dirname + '/views/personal'}) });
+app.get('/', function(req, res){
 
-
+    res.sendFile("index.html");
+})
 
 app.listen(8080);
